1145212db8265e50ab93d698a90a594d33238b1b
Kal Ahmed <kal.ahmed@epimorphics.com>
2024-11-08 15:30:25 +0000

    Add generated data for COSMOS instrumentation variables

---
+++ sample_data/templates/extensions.py
@@ -0,0 +1,14 @@
+from lib.template_state import TemplateState
+from lib.template_support import register_fn, uri_expand
+from rdflib import Literal
+
+def slug(text: str, state: TemplateState):
+    return '-'.join(text.lower().split())
+
+def with_datatype(text: str, state: TemplateState, dt: str):
+    dt_uri = uri_expand(dt, state.spec.namespaces, state)
+    if dt_uri is not None:
+        return Literal(text, datatype=dt_uri)
+
+register_fn('slug', slug)
+register_fn('withDatatype', with_datatype)
