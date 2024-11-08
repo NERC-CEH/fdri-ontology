8570708daf1660ed06066a83ce20f2fb387eed23
Kal Ahmed <kal.ahmed@epimorphics.com>
2024-11-08 12:52:52 +0000

    Add conversions for land cover data

---
+++ sample_data/sql/landCoverLcm.sql
@@ -0,0 +1,7 @@
+CREATE TABLE LC AS FROM read_csv('./src/LAND_COVER_LCM.csv', AUTO_DETECT=true) ;
+COPY (
+    SELECT *,
+    ROUND(FOOTPRINT / 100, 3) as AREA_RATIO,
+    make_date(CAST(YEAR AS INT), 1, 1) AS START_DATE
+    FROM LC
+) TO './build/landCoverLcm.csv' (HEADER, DELIMITER ',') ;
