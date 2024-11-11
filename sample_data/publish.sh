cdbf11b5fd1c6ea43587ff9773e897402195ae18
Mihajlo Milosavljevic <mihajlo.milosavljevic@epimorphics.com>
2024-11-11 12:54:58 +0000

    Actually write to s3 and publish to sqs queue via publish.sh

--- sample_data/publish.sh
+++ sample_data/publish.sh
@@ -25,10 +25,10 @@ do
   # Upload data to s3
   S3_DESTINATION=$S3_DESTINATION_PREFIX/${file#"build/"}
   echo Uploading $file to $S3_DESTINATION
-  # aws cp $file $S3_DESTINATION
+  aws cp $file $S3_DESTINATION
 
   # Publish data to sqs queue
   BODY=$(printf '{"payload":"%s","action":"replace-graph","context":"http://fdri.ceh.ac.uk/graph/%s","content-type":"text/turtle"}' $S3_DESTINATION ${file#"build/"})
   echo "Sending $BODY to $QUEUE_URL"
-  # aws sqs send-message --queue-url=$QUEUE_URL --message-body="$BODY"
+  aws sqs send-message --queue-url=$QUEUE_URL --message-body="$BODY"
 done
