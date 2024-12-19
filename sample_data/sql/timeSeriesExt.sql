1db7ee9b5084e51d23acc9a23839dead5f7d8522
Kal Ahmed <kal.ahmed@epimorphics.com>
2024-12-19 11:28:32 +0000

    Update sample data generation

--- sample_data/sql/timeSeriesExt.sql
+++ sample_data/sql/timeSeriesExt.sql
@@ -1,7 +1,12 @@
 CREATE TABLE TS AS FROM read_csv('./src/TIMESERIES.csv', AUTO_DETECT=true) ;
 CREATE TABLE ID AS FROM read_csv('./src/intervalDuration.csv', AUTO_DETECT=true) ;
 COPY (
-    SELECT TS.*, ID.DURATION FROM
-    TS
+    SELECT TS.*, ID.DURATION,
+    CASE 
+        WHEN TS.TABLE_NAME ^@ 'LEVEL1' THEN 1
+        WHEN TS.TABLE_NAME ^@ 'LEVEL2' THEN 2
+        WHEN TS.TABLE_NAME ^@ 'LEVEL3' THEN 3
+    END as PROCESSING_LEVEL,
+    FROM TS
     JOIN ID on TS.INTERVAL_ID=ID.INTERVAL_ID
 ) TO './build/timeSeriesExt.csv'
