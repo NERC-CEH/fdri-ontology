2adb0765f901350c93c009d810b5a07de9369d83
Kal Ahmed <kal.ahmed@epimorphics.com>
2024-12-20 10:15:35 +0000

    Add global SHACL validation and fix some of the reported issues

--- sample_data/validate-local.sh
+++ sample_data/validate-local.sh
@@ -1,3 +1,3 @@
 curl -X POST "http://localhost:3030/ds/shacl?graph=union" \
-    --data-binary @build/fdri_shacl.ttl \
+    --data-binary @build/fdri_shacl_with_refs.ttl \
     --header "Content-Type: application/turtle" > build/shacl_results.ttl
